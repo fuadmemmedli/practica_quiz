2be95b75cf2b887dea1dea62c49253fece94625d
Fuad Memedli <fuadmemedli@Fuad-MacBook-Air.local>
2022-12-16 10:33:03 +0400

    all category code added

---
+++ app.js
@@ -0,0 +1,31 @@
+// const homeBtn = document.getElementsByClassName('home__navbar__bottom__btn')
+// const homeCategory = document.getElementsByClassName('home__top__body__left')
+// const homeBtn = document.getElementsByClassName('home__navbar__bottom__btn').addEventListener("click", myFunc)
+
+// function myFunc() {
+//     document.getElementsByClassName('home__top__body__left').style.display='block'
+//   }
+
+//   var x = document.getElementsByClassName("home__navbar__bottom__btn");
+// x.addEventListener("click", function(){ 
+//     slm(document.getElementById("home__navbar__bottom__btn")); });
+
+
+// function slm(dis) {
+//     document.getElementsByClassName('home__navbar__bottom__btn').style.display="block";
+// }
+
+
+
+
+// var x = document.getElementById("n1");
+// x.addEventListener("mouseover", function(){ 
+//     mOverImg(document.getElementById("n1")); });
+// x.addEventListener("mouseout", function(){ mOutImg(document.getElementById("n1")); });
+
+// function mOverImg(img) {
+//     img.style.opacity="0";
+// }
+
+// function mOutImg(img) {
+//     img.style.opacity="1";
---
+++ jq.js
@@ -0,0 +1,11 @@
+$(function(){
+        $('.home__navbar__bottom__btn').on('click',()=>{
+          $( ".home__top__body__left" ).slideToggle();
+          
+        })
+      })
+
+//         $('.home__navbar__bottom__btn').on(('click'),()=>{
+//       alert('salam')
+//     }
+//   )
