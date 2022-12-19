d5c7f4301d829b50ab73ad1c814c428cb1951bfd
Fuad Memedli <fuadmemedli@Fuad-MacBook-Air.local>
2022-12-19 10:44:26 +0400

    card code added

--- app.js
+++ app.js
@@ -17,7 +17,65 @@ sldBtn.forEach(button => {
     })
 })
 
+const el = document.querySelector('.home__body__card4');
+
+const hiddenDiv = document.querySelector('#home__body__card4__icon');
+
+el.addEventListener('mouseover', function handleMouseOver() {
+    hiddenDiv.style.display = 'block';
+});
+
+
+el.addEventListener('mouseout', function handleMouseOut() {
+    hiddenDiv.style.display = 'none';
+
+  });
+  
+
+  const icn = document.querySelector('.home__body__card1');
+
+const hiddDiv = document.querySelector('#home__body__card1__icon');
+
+icn.addEventListener('mouseover', function hanMouseOver() {
+    hiddDiv.style.display = 'block';
+});
+
+
+icn.addEventListener('mouseout', function hanMouseOut() {
+    hiddDiv.style.display = 'none';
+
+  });
 
 
 
 
+
+  const ic = document.querySelector('.home__body__card2');
+
+const hidDiv = document.querySelector('#home__body__card2__icon');
+
+ic.addEventListener('mouseover', function hanMouseOver() {
+    hidDiv.style.display = 'block';
+});
+
+
+ic.addEventListener('mouseout', function hanMouseOut() {
+    hidDiv.style.display = 'none';
+
+  });
+  
+
+  const ele = document.querySelector('.home__body__card3');
+
+const hiddenDivv = document.querySelector('#home__body__card3__icon');
+
+ele.addEventListener('mouseover', function handleMouseOver() {
+    hiddenDivv.style.display = 'block';
+});
+
+
+ele.addEventListener('mouseout', function handleMouseOut() {
+    hiddenDivv.style.display = 'none';
+
+  });
+  
